71ae4e1666cdffbb27bf5adf4a6cab712b7a8af4
PierreWTH <wietrichpierre@gmail.com>
2024-03-05 11:25:17 +0100

    add create post form

---
+++ components/Forms/CreatePostForm.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import { InputText } from "../InputText/InputText";
+import { Button } from "../Button/Button";
+
+export default function CreatePostForm() {
+  return (
+    <View>
+      <Text style={styles.h1}>Créer un post</Text>
+      <InputText label="Titre : " />
+      <InputText height={150} label="Contenu : " />
+      <Button label="Créer" backgroundColor="black" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  h1: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
